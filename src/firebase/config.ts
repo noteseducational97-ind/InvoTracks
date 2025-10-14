99669e0339093a3ce7bc54d6076ecc1c38545e27
Prasanna Warade <prasannawarade0204@gmail.com>
2025-10-14 11:50:07 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6610342034-61175",
+  "appId": "1:458181401227:web:a99c0f672cbe790220928e",
+  "apiKey": "AIzaSyCkYJqCOSfIm_yGPIC8TERkqCqJ9z01WTc",
+  "authDomain": "studio-6610342034-61175.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "458181401227"
+};
