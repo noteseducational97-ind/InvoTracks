8128ef076be0b32b1fc509a0e8c4b15849bbe006
Prasanna Warade <prasannawarade0204@gmail.com>
2025-10-15 04:54:16 +0000

    remove all content from investments page (_for element div_)

---
+++ src/app/(dashboard)/dashboard/investments-page.tsx
@@ -0,0 +1,9 @@
+
+export default function InvestmentsPage() {
+    return (
+        <div>
+            <h1 className="font-headline text-3xl font-bold tracking-tight">Your Investments</h1>
+            <p className="text-muted-foreground">Here is a detailed view of your portfolio.</p>
+        </div>
+    );
+}
